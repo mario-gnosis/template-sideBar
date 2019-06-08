9da08df47858ef370b5fd8a444e577101c4dd525
Mário N. Lima <mario_gnosis@yahoo.com.br>
2019-06-08 12:55:47 +0100

    template rascunho

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormComponent } from './components/form/form.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { OrdersListComponent } from './components/orders/orders-list/orders-list.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    OrdersComponent,
+    OrdersListComponent,
+    MainMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main-menu/main-menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit  } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.css']
+})
+export class MainMenuComponent implements OnInit  {
+
+
+  constructor() {
+
+   
+  }
+
+
+  ngOnInit() {
+  
+  
+  }
+
+}
